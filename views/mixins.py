ddb638320859c0f29062148b924d7d56b60efce1
Liljack118 <kamilzajaczek4@gmail.com>
2022-09-07 18:33:23 +0200

    create ReceiveTranscriptionMixin
    this mixin have methods that will be usefull when formating request with transcription and creating transcription docx file

--- views/mixins.py
+++ views/mixins.py
@@ -1,9 +1,18 @@
+import requests
+from docx import Document as docx_document
+import tempfile
+import io
+from django.core.files.base import File
 from wagtail.documents.models import Document
 import re
 import urllib
 from django.apps import apps
 from PIL import ImageFile
 from django.urls import reverse
+from django.utils.html import format_html
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+
 
 class TranscriptionDataValidationMixin:
     def data_validation(self, video_id, model_instance_str, transcription_field):
@@ -58,3 +67,84 @@ class TranscriptionDataValidationMixin:
                 break
         file.close()
         return None
+
+class ReceiveTranscriptionMixin:
+
+    def get_user(self, user_id):
+        return get_object_or_404(User, id=user_id)
+
+    def get_model_instance(self, model_instance_str):
+        try:
+            app, model, instance_id = model_instance_str.split(':')
+            model_instance = apps.get_model(app, model).objects.get(id=str(instance_id))
+            return model_instance
+        except (AttributeError, ValueError, LookupError):
+            return None
+
+    def get_transcription(self, transcript_id):
+        # GET TRANSCRIPED FILE
+        endpoint = f"https://api.assemblyai.com/v2/transcript/{transcript_id}"
+        headers = {
+            "authorization": self.api_token,
+        }
+        r = requests.get(endpoint, headers=headers)
+        return r.json()
+
+    def format_start_time(self, start):
+        # FUNCTION TO FORMAT START TIME
+        hours, start = start//3600000, start - (start//3600000) * 3600000
+        minutes, start = start//60000, start - (start//60000) * 60000
+        seconds, start = start//1000, start - (start//1000) * 1000
+        miliseconds = start
+        return f"[{hours:02d}:{minutes:02d}:{seconds:02d}.{miliseconds}]"
+
+    def get_transcription_devided_by_phrases(self, words):
+        phrases, phrase, speaker = [], [], None
+        for word in words:
+            if not speaker: 
+                speaker = {'start':self.format_start_time(word['start']), 'speaker':word['speaker']}
+
+            if speaker['speaker'] != word['speaker']:
+                phrases.append({**speaker, 'phrase':' '.join(phrase)})
+                speaker = {'start':self.format_start_time(word['start']), 'speaker':word['speaker']}
+                phrase = [word['text']]
+            
+            elif speaker['speaker'] == word['speaker']:
+                phrase.append(word['text'])
+        phrases.append({**speaker, 'phrase':' '.join(phrase)})
+        return phrases
+
+    def transcription_phrases_to_docx(self, transcription_phrases):
+        document = docx_document()
+        with tempfile.NamedTemporaryFile() as tmp:
+            for phrase in transcription_phrases:
+                start_paragraph = document.add_paragraph()
+                run = start_paragraph.add_run(str(phrase.get("start")))
+                run.bold = True
+                document.add_paragraph(phrase.get("phrase") + '\n')
+
+            document.save(tmp.name)
+            io_output = io.BytesIO(tmp.read())
+        return io_output
+
+    def add_docx_to_wagtail_docs(self, io_output, video_id):
+        docx_file = File(io_output, name=f'auto_transcription-{video_id}.docx')
+        document_file = Document.objects.create(title=f"auto_transcription-{video_id}", file=docx_file)
+        return document_file
+
+    def get_notification_message(self, transcription_document=None, error=False, edit_url=None, video_id=None):
+        """
+        This method is used to create notification popup displayed for user
+        when transcription is done
+        """
+        message = format_html(f"""
+            <div class="notification-header {'error' if error else ''}">
+                <p class="notification-header-text"><i class="bi bi-square-fill"></i><b style="margin: auto 0;">{'Error During Transcription Process' if error else 'New Transcription'}</b><p>
+                <p class="notification-close" data-action_url={reverse("wagtail_transcription:delete_notification")}><i class="bi bi-x"></i></p>
+            </div>
+            <div class="notification-message {'error' if error else ''}">
+                <a target="_blank" href="{edit_url}">Check Session Page</a>
+                <a target="_blank" href="{f'https://www.youtube.com/watch?v={video_id}' if error else transcription_document.url}">{'Check video' if error else 'Download Transcription File <i class="bi bi-download"></i>'}</a>
+            </div>
+        """)
+        return message
