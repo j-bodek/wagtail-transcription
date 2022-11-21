1ee46d3c3ba8fb695172e84d83742b5a5d87da5f
Liljack118 <kamilzajaczek4@gmail.com>
2022-11-21 23:30:24 +0100

    restructure rest transcription views

---
+++ wagtail_transcription/views/transcription/helpers.py
@@ -0,0 +1,83 @@
+# django
+from django.http import JsonResponse, HttpRequest
+from django.views import View
+from django.utils.html import format_html
+from django.shortcuts import reverse, get_object_or_404
+from django.conf import settings
+from django.middleware import csrf
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from django.db.models import Model
+from django.db import transaction
+from django.template import loader
+from django.apps import apps
+
+# notifications
+from notifications.signals import notify
+
+# pytube
+from pytube import YouTube
+
+# wagtail transcription
+# from wagtail_transcription.views.mixins import ReceiveTranscriptionMixin
+from wagtail_transcription.decorators import video_data_validation
+from wagtail_transcription.models import Transcription
+from wagtail_transcription.wagtail_hooks import TranscriptionAdmin
+from wagtail_transcription.tokens import validated_video_data_token
+
+# other packages
+import requests
+import json
+import re
+import time
+from typing import Type
+import logging
+
+
+class GetProcessingTranscriptionsView(View):
+    """
+    Return ids of precessing transcriptions
+    """
+
+    def get(self, request, *args, **kwargs):
+        transcriptions_video_ids = list(
+            Transcription.objects.filter(completed=False).values_list(
+                "video_id", flat=True
+            )
+        )
+
+        transcriptions_video_ids = {
+            video_id: True for video_id in transcriptions_video_ids
+        }
+        return JsonResponse(transcriptions_video_ids)
+
+
+class GetTranscriptionData(View):
+    """
+    Return transcription id, title and edit url
+    based od video_id
+    """
+
+    def get(self, request, *args, **kwargs):
+        video_id = request.GET.get("video_id")
+        yt_id_regex = re.compile(r"^[a-zA-Z0-9_-]{11}$")
+        if not yt_id_regex.match(str(video_id)):
+            transcription = None
+        else:
+            transcription = get_object_or_404(Transcription, video_id=video_id)
+
+        return JsonResponse(
+            {
+                "new_transcription_id": None if not transcription else transcription.id,
+                "new_transcription_title": None
+                if not transcription
+                else transcription.title,
+                "new_transcription_edit_url": None
+                if not transcription
+                else TranscriptionAdmin().url_helper.get_action_url(
+                    "edit", transcription.id
+                ),
+            }
+        )
---
+++ wagtail_transcription/views/transcription/validation.py
@@ -0,0 +1,111 @@
+# django
+from django.http import JsonResponse, HttpRequest
+from django.views import View
+from django.utils.html import format_html
+from django.shortcuts import reverse, get_object_or_404
+from django.conf import settings
+from django.middleware import csrf
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from django.db.models import Model
+from django.db import transaction
+from django.template import loader
+from django.apps import apps
+
+# notifications
+from notifications.signals import notify
+
+# pytube
+from pytube import YouTube
+
+# wagtail transcription
+# from wagtail_transcription.views.mixins import ReceiveTranscriptionMixin
+from wagtail_transcription.decorators import video_data_validation
+from wagtail_transcription.models import Transcription
+from wagtail_transcription.wagtail_hooks import TranscriptionAdmin
+from wagtail_transcription.tokens import validated_video_data_token
+
+# other packages
+import requests
+import json
+import re
+import time
+from typing import Type
+import logging
+
+
+class ValidateTranscriptionDataView(View):
+    """
+    Validate video_id, model_instance_str, check if transcription for same video is running
+    If data valid return modal with video title, link, thumbnail, channel name and button
+    to continue. If data invalid return modal with appropriate error message
+
+    This view get following data via post request:
+    -   csrfmiddleware token - django token used to protect from
+        cross site request forgery attacks
+
+    -   video_id - id of youtube video
+
+    -   parent_instance_str - string used to get instance of parent model (model
+        to which transcription will be assigned)
+
+    -   transcription_field - parent model field which willbe used
+        to set transcription instance
+
+    -   transcription_field_id - html id of field to which
+        transcription will be associated
+
+    -   field_name - name of field for youtube id
+    """
+
+    http_method_names = ["post"]
+
+    @method_decorator(video_data_validation)
+    def post(
+        self,
+        request: Type[HttpRequest],
+        youtube_instance: Type[YouTube],
+        *args,
+        **kwargs,
+    ) -> Type[JsonResponse]:
+
+        self.youtube_instance = youtube_instance
+        data = self.get_context_data(request.POST)
+        return JsonResponse(data)
+
+    def get_context_data(self, data: dict) -> dict:
+        """
+        return data that will be returned by JsonResponse
+        """
+
+        # get video data (title, thumbnail, channel etc)
+        url = f"https://www.googleapis.com/youtube/v3/videos?part=snippet&id={data.get('video_id')}&key={settings.YOUTUBE_DATA_API_KEY}"
+        r = requests.get(url)
+        snippet = r.json()["items"][0]["snippet"]
+
+        # generate video info popup content
+        message = loader.render_to_string(
+            "wagtail_transcription/components/transcription_info_popup.html",
+            context={
+                "token": validated_video_data_token.make_token(
+                    self.request.user, data.get("video_id")
+                ),
+                "audio_duration": time.strftime(
+                    "%H:%M:%S", time.gmtime(self.youtube_instance.length // 1.25)
+                ),
+                "video_title": snippet["title"],
+                "video_thumbnail": snippet["thumbnails"]["default"]["url"],
+                "channel_name": snippet["channelTitle"],
+                "audio_url": self.youtube_instance.streams[0].url,
+                **{k: data.get(k) for k, _ in data.items()},
+            },
+            request=self.request,
+        )
+
+        return {
+            "class": "success",
+            "type": "success",
+            "message": message,
+        }
