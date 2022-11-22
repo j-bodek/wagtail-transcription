ce29df6af2f701706c08a6d63a279cf44348ab51
Liljack118 <kamilzajaczek4@gmail.com>
2022-11-22 21:06:44 +0100

    split menu register logic into two methods, remove html from py file

--- wagtail_transcription/wagtail_hooks.py
+++ wagtail_transcription/wagtail_hooks.py
@@ -10,11 +10,16 @@ from django.utils.translation import gettext_lazy as _
 from django.shortcuts import reverse
 from wagtail.admin.menu import Menu, SubmenuMenuItem
 from django.conf import settings
-from django.utils.html import format_html
+from django.template import loader
 import requests
+from typing import Type
 
 
 class TranscriptionAdmin(ModelAdmin):
+    """
+    This class define Transcription Admin
+    """
+
     model = Transcription
     menu_icon = "doc-full-inverse"
     base_url_path = "transcription"
@@ -28,99 +33,77 @@ class TranscriptionAdmin(ModelAdmin):
     list_filter = ["completed", "verified"]
     search_fields = ["title"]
 
-    def video(self, obj):
+    def video(self, obj: Type[Transcription]):
+        """
+        This method is responsible for displaying formated video
+        data (thumbnail, title, url)
+        """
+
         url = f"https://www.googleapis.com/youtube/v3/videos?part=snippet&id={obj.video_id}&key={settings.YOUTUBE_DATA_API_KEY}"
         r = requests.get(url)
+        context = {"obj": obj}
         try:
             snippet = r.json()["items"][0]["snippet"]
-            snippet = f"""
-            <img src='{snippet["thumbnails"]["default"]["url"]}' width="60"/>
-            <p style="font-size:10px; margin-left: .5rem">{snippet["title"]}</p>
-            """
+            context.update(
+                {
+                    "thumbnail": snippet["thumbnails"]["default"]["url"],
+                    "title": snippet["title"],
+                }
+            )
         except IndexError:
-            snippet = "Video Url"
+            context.update({"thumbnail": None, "title": None})
 
-        return format_html(
-            f"""
-            <a target="_blank" style="display:flex; max-width:200px"
-            href="https://www.youtube.com/watch?v={obj.video_id}">
-            {snippet}
-            </a>
-        """
+        return loader.render_to_string(
+            "wagtail_transcription/admin/video_data.html",
+            context=context,
         )
 
-    def register_with_wagtail(self):
-        @hooks.register("register_permissions")
-        def register_permissions():
-            return self.get_permissions_for_registration()
-
-        @hooks.register("register_admin_urls")
-        def register_admin_urls():
-            return self.get_admin_urls_for_registration()
-
-        menu_hook = (
-            "register_settings_menu_item"
-            if self.add_to_settings_menu
-            else "register_admin_menu_item"
-        )
-
+    def get_menu_item(self):
         if hasattr(settings, "DOCUMENTS_GROUP") and settings.DOCUMENTS_GROUP is True:
             """
-            Hide Documents menu item and create custom documents group
+            If DOCUMENTS_GROUP menu variable is set to True merge documents
+            and tanscription into one menu group
             """
 
+            # remove documents snippet
             @hooks.register("construct_main_menu")
             def hide_snippets_menu_item(request, menu_items):
                 menu_items[:] = [
                     item for item in menu_items if item.name != "documents"
                 ]
 
-            @hooks.register(menu_hook)
-            def register_admin_menu_item():
-                menu_items = [
-                    DocumentsMenuItem(
-                        _("Documents"),
-                        reverse("wagtaildocs:index"),
-                        name="documents",
-                        icon_name="doc-full-inverse",
-                        order=400,
-                    ),
-                    self.get_menu_item(),
-                ]
-                if WAGTAIL_VERSION >= (4, 0):
-                    submenu = Menu(items=menu_items)
-                else:
-                    from wagtail.contrib.modeladmin.menus import SubMenu
+            # create submenu item
+            menu_items = self.get_submenu_items()
+            if WAGTAIL_VERSION >= (4, 0):
+                submenu = Menu(items=menu_items)
+            else:
+                from wagtail.contrib.modeladmin.menus import SubMenu
 
-                    submenu = SubMenu(menu_items)
-                return SubmenuMenuItem(
-                    "Documents",
-                    submenu,
-                    classnames="doc-full-inverse",
-                    name="custom-documents",
-                )
-
+                submenu = SubMenu(menu_items)
+            return SubmenuMenuItem(
+                "Documents",
+                submenu,
+                classnames="doc-full-inverse",
+                name="custom-documents",
+            )
         else:
-            """
-            Create Transcription menu item
-            """
+            return super().get_menu_item()
 
-            @hooks.register(menu_hook)
-            def register_admin_menu_item():
-                return self.get_menu_item()
+    def get_submenu_items(self) -> list:
+        """
+        returns list of submenu items
+        """
 
-        # Overriding the explorer page queryset is a somewhat 'niche' / experimental
-        # operation, so only attach that hook if we specifically opt into it
-        # by returning True from will_modify_explorer_page_queryset
-        if self.will_modify_explorer_page_queryset():
-
-            @hooks.register("construct_explorer_page_queryset")
-            def construct_explorer_page_queryset(parent_page, queryset, request):
-                return self.modify_explorer_page_queryset(
-                    parent_page, queryset, request
-                )
-
-        self.register_admin_url_finders()
+        return [
+            DocumentsMenuItem(
+                _("Documents"),
+                reverse("wagtaildocs:index"),
+                name="documents",
+                icon_name="doc-full-inverse",
+                order=400,
+            ),
+            super().get_menu_item(),
+        ]
 
 
 modeladmin_register(TranscriptionAdmin)
